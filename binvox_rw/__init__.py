bf969bcd29c7a2d94b92e04ec200cbe5dcc04794
Wang Qiang <37444407+wangqiang9@users.noreply.github.com>
2024-01-22 17:21:03 +0800

    Create __init__.py

---
+++ binvox_rw/__init__.py
@@ -0,0 +1,9 @@
+# This is the __init__.py file for the src module
+
+from .binvox_rw import Voxels
+from .binvox_rw import read_header
+from .binvox_rw import read_as_3d_array
+from .binvox_rw import read_as_coord_array
+from .binvox_rw import dense_to_sparse
+from .binvox_rw import sparse_to_dense
+from .binvox_rw import write
